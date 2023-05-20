88a442e224f4670a2e339dba541946f1e97afa99
Geraldo Francisco Conselho <geraldof.neto2016@gmail.com>
2023-05-20 13:17:13 -0300

    mandando mensagem texto pelo backend

---
+++ src/main/java/com/thor/telegram/dto/integration/TextIntegrationRequest.java
@@ -0,0 +1,17 @@
+package com.thor.telegram.dto.integration;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TextIntegrationRequest {
+    @JsonProperty("chat_id")
+    private String chat;
+    private String text;
+}
--- src/main/java/com/thor/telegram/integration/MessageIntegration.java
+++ src/main/java/com/thor/telegram/integration/MessageIntegration.java
@@ -1,21 +1,44 @@
 package com.thor.telegram.integration;
 
+import com.thor.telegram.dto.integration.TextIntegrationRequest;
 import com.thor.telegram.model.MessageDomain;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Mono;
 
 @Component
 @RequiredArgsConstructor
 public class MessageIntegration {
-    @Value("app.telegram-api.base")
-    private String url;
+    @Value("${app.telegram-api.base}")
+    private String base;
+
+    @Value("${app.telegram-api.send-text}")
+    private String sendText;
+
+    @Value("${app.telegram-api.send-photo}")
+    private String sendPhoto;
 
     private final WebClient webClient;
 
     public Mono<Void> sendTextMessage(MessageDomain domain) {
-        return Mono.empty();
+        var url = UriComponentsBuilder.fromUriString(base)
+                .path(sendText).buildAndExpand("6068453482:AAFkjxaVc7MUR05cf163pe4gpx72aSLGZxw").toString();
+
+        var request = TextIntegrationRequest.builder()
+                .text(domain.getMessage())
+                .chat(domain.getChat())
+                .build();
+
+        return webClient
+                .post()
+                .uri(url)
+                .bodyValue(request)
+                .retrieve()
+                .toBodilessEntity()
+                .then();
     }
 }
