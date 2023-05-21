b4a5a7278d871ba5ce196bf3883443548a22ce97
Geraldo Francisco Conselho <geraldof.neto2016@gmail.com>
2023-05-21 14:14:37 -0300

    enviando arquivo base64 para o telegram

--- src/main/java/com/thor/telegram/integration/MessageIntegration.java
+++ src/main/java/com/thor/telegram/integration/MessageIntegration.java
@@ -6,17 +6,26 @@ import com.thor.telegram.dto.message.MessageRequest;
 import com.thor.telegram.mapper.MessageMapper;
 import com.thor.telegram.model.MessageDomain;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.data.util.Pair;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Mono;
 
+import java.util.Base64;
+
+import static com.thor.telegram.enumereble.MessageType.PHOTO;
 import static com.thor.telegram.enumereble.MessageType.TEXT;
 import static java.time.Duration.ofSeconds;
-import static java.time.LocalDateTime.now;
+import static org.springframework.http.MediaType.MULTIPART_FORM_DATA;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class MessageIntegration extends IntegrationBase {
@@ -39,12 +48,17 @@ public class MessageIntegration extends IntegrationBase {
                 .chat(pair.getFirst().getChat())
                 .build();
 
+        var uri = UriComponentsBuilder
+                .fromUriString(base)
+                .path(sendText).buildAndExpand(pair.getSecond().getBot().getToken())
+                .toString();
+
         return Mono.just(pair.getFirst())
                 .map(m -> mapper.toDomain(m, TEXT))
                 .flatMap(m ->
                         webClient
                                 .post()
-                                .uri(getUri(pair.getSecond().getBot().getToken(), sendText))
+                                .uri(uri)
                                 .bodyValue(request)
                                 .retrieve()
                                 .toBodilessEntity()
@@ -58,7 +72,41 @@ public class MessageIntegration extends IntegrationBase {
                 );
     }
 
-    private String getUri(String token, String path) {
-        return UriComponentsBuilder.fromUriString(base).path(path).buildAndExpand(token).toString();
+    public Mono<MessageDomain> sendPhotoMessage(Pair<MessageRequest, ChatResponse> pair) {
+
+        var uri = UriComponentsBuilder
+                .fromUriString(base)
+                .path(sendPhoto)
+                .queryParam("chat_id", pair.getSecond().getName())
+                .buildAndExpand(pair.getSecond().getBot().getToken()).toString();
+
+
+        return Mono.just(pair.getFirst())
+                .map(m -> mapper.toDomain(m, PHOTO))
+                .flatMap(m ->
+                        webClient
+                                .post()
+                                .uri(uri)
+                                .contentType(MULTIPART_FORM_DATA)
+                                .bodyValue(getFile(pair.getFirst().getMessage()))
+                                .retrieve()
+                                .toBodilessEntity()
+                                .timeout(ofSeconds(this.timeout))
+                                .retryWhen(this.retry())
+                                .onErrorResume(e -> {
+                                    m.setSent(false);
+                                    return Mono.empty();
+                                })
+                                .thenReturn(m)
+
+                );
     }
+
+    private MultiValueMap<String, HttpEntity<?>> getFile(String base64) {
+        var array = Base64.getDecoder().decode(base64);
+        var builder = new MultipartBodyBuilder();
+        builder.part("photo", new ByteArrayResource(array)).filename("file");
+        return builder.build();
+    }
+
 }
